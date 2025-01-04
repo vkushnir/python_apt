6db73cb01c89d499c12a45cf355adb98be3210ce
Vladimir Kushnir <vkushnir@gmail.com>
2025-01-04 23:47:54 +0300

    refactor: move some functions to outside.

--- pyapt/pkg/contrib/platformutil.py
+++ pyapt/pkg/contrib/platformutil.py
@@ -6,79 +6,94 @@ import re
 from .singleton import Singleton
 
 
+def arch_to_dpkg(arch: str) -> str:
+    """
+    Convert architecture to dpkg format
+    """
+    return {
+        'x86_64': 'amd64',
+        'aarch64': 'arm64',
+        'armv7l': 'armhf',
+        'armv6l': 'armel',
+        'armv5tel': 'arm',
+        'i386': '386',
+        'i686': '386'
+    }.get(arch, arch)
+
+
+def get_debian_os_release(os_release_file: str = '/etc/os-release') -> dict:
+    os_release = {}
+    with open(os_release_file, 'r') as f:
+        for line in f:
+            if line.strip().startswith('#'):
+                continue
+            key, value = line.strip().split('=')
+            os_release[key] = value
+    return os_release
+
+
+def _get_mac_system(system_version_file: str = '/System/Library/CoreServices/SystemVersion.plist') -> dict:
+    system_version = {}
+    with open(system_version_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.startswith('<key>'):
+                key = re.search(r'<key>(.*)</key>', line).group(1)
+            elif line.startswith('<string>'):
+                value = re.search(r'<string>(.*)</string>', line).group(1)
+                system_version[key] = value
+    return system_version
+
+
+def get_mac_codename(release: str) -> str:
+    releases = {
+        '24': 'Sequoia',
+        '23': 'Sonoma',
+        '22': 'Ventura',
+        '21': 'Monterey',
+        '20': 'Big Sur',
+        '19': 'Catalina',
+        '18': 'Mojave',
+        '17': 'High Sierra',
+        '16': 'Sierra',
+        '15': 'El Capitan',
+        '14': 'Yosemite',
+        '13': 'Mavericks',
+        '12': 'Mountain Lion',
+        '11': 'Lion',
+        '10': 'Snow Leopard',
+        '9': 'Leopard',
+        '8': 'Tiger',
+        '7': 'Panther',
+        '6': 'Jaguar'
+    }
+    darwin_version = release.split('.')[0]
+    return releases.get(darwin_version, 'Unknown')
+
+
 # Class to detect system parameters
 class PlatformInfo(Singleton):
     def __init__(self):
         self._os = platform.system()
-        self._arch = platform.machine()
+        self._arch = arch_to_dpkg(platform.machine())
         self._version = platform.version()
         self._release = platform.release()
         if self._os == 'Linux':
-            self._system = self._get_debian_system()
+            self._system = get_debian_os_release()
             self._id = self._system['ID']
             self._version = self._system['VERSION']
             self._distro = self._system['VERSION_CODENAME']
         elif self._os == 'Darwin':
-            self._system = self._get_mac_system()
+            self._system = _get_mac_system()
             self._id = self._system['ProductName']
             self._version = self._system['ProductUserVisibleVersion']
-            self._distro = self._get_mac_codename(self._release)
+            self._distro = get_mac_codename(self._release)
         else:
             self._system = None
             self._id = 'Unknown'
             self._version = 'Unknown'
             self._distro = 'Unknown'
         self._python_version = platform.python_version()
-    
-    @staticmethod
-    def _get_debian_system(self) -> dict:
-        os_release = {}
-        with open('/etc/os-release', 'r') as f:
-            for line in f:
-                if line.strip().startswith('#'):
-                    continue
-                key, value = line.strip().split('=')
-                os_release[key] = value
-        return os_release
-    
-    @staticmethod
-    def _get_mac_system() -> dict:
-        system_version = {}
-        with open('/System/Library/CoreServices/SystemVersion.plist', 'r') as f:
-            for line in f:
-                line = line.strip()
-                if line.startswith('<key>'):
-                    key = re.search(r'<key>(.*)</key>', line).group(1)
-                elif line.startswith('<string>'):
-                    value = re.search(r'<string>(.*)</string>', line).group(1)
-                    system_version[key] = value
-        return system_version
-    
-    @staticmethod
-    def _get_mac_codename(release: str) -> str:
-        releases = {
-            '24': 'Sequoia',
-            '23': 'Sonoma',
-            '22': 'Ventura',
-            '21': 'Monterey',
-            '20': 'Big Sur',
-            '19': 'Catalina',
-            '18': 'Mojave',
-            '17': 'High Sierra',
-            '16': 'Sierra',
-            '15': 'El Capitan',
-            '14': 'Yosemite',
-            '13': 'Mavericks',
-            '12': 'Mountain Lion',
-            '11': 'Lion',
-            '10': 'Snow Leopard',
-            '9': 'Leopard',
-            '8': 'Tiger',
-            '7': 'Panther',
-            '6': 'Jaguar'
-        }
-        darwin_version = release.split('.')[0]
-        return releases.get(darwin_version, 'Unknown')
     
     @property
     def os(self) -> str:
